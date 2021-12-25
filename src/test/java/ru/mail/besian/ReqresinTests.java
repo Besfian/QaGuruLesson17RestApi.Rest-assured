25b4aee47d1ba699ef392820036b4e5dbaaac70d
besfian <besfian@mail.ru>
2021-12-25 14:17:53 +0300

    Подправлена стилистика и добавлен пакет

--- src/test/java/ru/mail/besian/ReqresinTests.java
+++ src/test/java/ru/mail/besian/ReqresinTests.java
@@ -1,3 +1,5 @@
+package ru.mail.besian;
+
 import io.restassured.RestAssured;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
@@ -13,6 +15,7 @@ public class ReqresinTests {
     static void prepare() {
         RestAssured.baseURI = "https://reqres.in/";
     }
+
     @Test
     void loginSuccessful() {
         String data = "{\"email\": \"eve.holt@reqres.in\",\"password\": \"pistol\" }";
@@ -22,9 +25,7 @@ public class ReqresinTests {
                 .body(data)
                 .when()
                 .post("api/login")
-                .then()
-                .log()
-                .all()
+                .then().log().all()
                 .body("token", is("QpwL5tke4Pnpja7X4"))
                 .statusCode(200);
 
@@ -40,9 +41,8 @@ public class ReqresinTests {
                 .body(requestData)
                 .when()
                 .patch("api/users/2")
-                .then()
-                .log()
-                .all().statusCode(200)
+                .then().log().all()
+                .statusCode(200)
                 .extract().response().toString();
         assertThat(responseData.equals(response));
 
@@ -58,17 +58,15 @@ public class ReqresinTests {
                 .body(requestData)
                 .when()
                 .post("api/users")
-                .then()
-                .log()
-                .all().statusCode(201)
+                .then().log().all()
+                .statusCode(201)
                 .extract().response().path("job");
         assertThat(response.equals(job));
 
     }
 
     @Test
-    void
-    delayedResponse() {
+    void delayedResponse() {
         String responseData = "{ \"id\": 6, \"email\": \"tracey.ramos@reqres.in\", \"first_name\": \"Tracey\", \"last_name\": \"Ramos\", \"avatar\": \"https://reqres.in/img/faces/6-image.jpg\" }";
         ArrayList response = RestAssured
                 .given().log().all()
@@ -76,27 +74,24 @@ public class ReqresinTests {
                 .queryParams("delay", 3)
                 .when()
                 .get("api/users")
-                .then()
-                .log()
-                .all().statusCode(200)
+                .then().log().all()
+                .statusCode(200)
                 .extract().response().path("data");
         assertThat(response).hasSize(6);
         assertThat(response.get(5)).toString().equals(responseData);
 
 
     }
+
     @Test
-    void
-    delete() {
+    void delete() {
         RestAssured
                 .given().log().all()
                 .contentType(JSON)
                 .when()
                 .delete("api/users/2")
-                .then()
-                .log()
-                .all().statusCode(204);
-
+                .then().log().all()
+                .statusCode(204);
 
 
     }
